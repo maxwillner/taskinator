e511aa3584cf6adfb978a1ebede95fea6d36b418
Frankenstein <88062514+maxwillner@users.noreply.github.com>
2021-08-25 17:04:47 -0400

    task persistent set_items

--- assets/js/script.js
+++ assets/js/script.js
@@ -62,6 +62,8 @@ var completeEditTask = function(taskName, taskType, taskId) {
 
     alert("Task Updated!");
 
+    saveTasks();
+
     formEl.removeAttribute("data-task-id");
     document.querySelector("#save-task").textContent = "Add Task";
 };
@@ -97,8 +99,7 @@ var createTaskEl = function(taskDataObj) {
     // increase task counter for next unique id
     taskIdCounter++;
 
-    console.log(taskDataObj);
-    console.log(taskDataObj.status);
+    saveTasks();
 };
 
 var createTaskActions = function (taskId) {
@@ -197,6 +198,8 @@ var deleteTask = function(taskId) {
 
     // reassign tasks array to be the same as UpdatedTaskArr
     tasks = updatedTaskArr;
+
+    saveTasks();
 };
 
 var taskStatusChangeHandler = function(event){
@@ -225,8 +228,12 @@ var taskStatusChangeHandler = function(event){
             tasks[i].status = statusValue;
         }
     }
-    console.log(tasks)
-}
-pageContentEl.addEventListener("click", taskButtonHandler);
 
+    saveTasks();
+}
+var saveTasks = function() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+pageContentEl.addEventListener("click", taskButtonHandler);
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
